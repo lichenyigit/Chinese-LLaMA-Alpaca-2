4c253f02b9ebc964620911988b1ed32589567b10
ChenYi·Li <lichenyiwin@outlook.com>
2023-09-15 01:01:44 +0800

    Update run_sft_lcy_test.sh

--- scripts/training/run_sft_lcy_test.sh
+++ scripts/training/run_sft_lcy_test.sh
@@ -13,7 +13,7 @@ per_device_train_batch_size=1
 per_device_eval_batch_size=1
 gradient_accumulation_steps=8
 output_dir=output_dir
-peft_model=path/to/peft/model/dir
+peft_model=/content/chinese-alpaca-2-lora-7b
 validation_file=validation_file_name.json
 
 deepspeed_config_file=ds_zero2_no_offload.json
